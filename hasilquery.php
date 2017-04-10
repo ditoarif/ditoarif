187cb84cc4a5bcba4d7bf0fd7fa9038232c1ab2c
ditoarif <ditoarif966@gmail.com>
2017-04-10 10:41:35 +0700

    Add files via upload

--- hasilquery.php
+++ hasilquery.php
@@ -16,10 +16,10 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-$hasil=$_POST[katakunci];
+$hasil=$_POST['katakunci'];
+$sql = "SELECT distinct nama_file,token,tokenstem FROM `dokumen` WHERE token like '%$hasil%'";
 //$sql = "SELECT distinct nama_file,token,tokenstem FROM `dokumen` where token like '%$hasil%'";
-$sql = "SELECT distinct nama_file,token,tokenstem FROM `dokumen` WHERE MATCH (token,tokenstem) AGAINST ('$hasil' IN BOOLEAN MODE)";
-echo $sql;
+
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
